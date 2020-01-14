724a97af6eac1ec19e4ef999cb15924812db9a38
Opilane TTHK <opilane@tthk.ee>
2020-01-14 11:54:30 +0200

    first commit

---
+++ github.js
@@ -0,0 +1,17 @@
+class GitHub{
+
+    constructor(){
+        this.client_ID = '1405d83a641097f0e38e';
+        this.cient_secret = 'd15536e2a1f0892df7c7d8801af5d4fc36a67592';
+        this.user = 'StevenKoska1';
+        this.apiUrl = "https://api.github.com/users/";
+        this.repos_count = 5;
+        this.repos_sort = 'created asc';
+    }
+
+    async getUserData(){
+        let urlProfile = `${this.apiUrl}${this.user}$client_id=${this.client_ID}&client_secret=${this.client_secret}`;
+        let urlRepos = `${this.apiUrl}${this.user}/repos?per_page=?${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_ID}&client_secret=${this.client_secret}`;
+    }
+
+}
